3d3edcb1eebdc03cdcf94df44282188bdcd9af76
Ammar Ozair <sabeenjamil100@yahoo.com>
2023-09-12 12:58:19 +0100

    Creation of team member classes and creating new instances based on user input

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
-
+const Engineer = require("./lib/Engineer");
+//Declare empty array for team
+teamArr = [];
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+function buildTeam(){
 
 inquirer
   .prompt([
     {
       name: 'name',
-      message: 'Please input Manager details. Enter Manager Name',
+      message: 'Hi. Welcome to the Team Profile Generator app. First please input Manager details. Enter Manager Name',
 
     },
     {
@@ -33,50 +35,106 @@ inquirer
       name: 'officeNumber',
       message: 'Please enter Office Number',
     },
-    {
-        type: 'rawlist',
-        name: 'option',
-        message: 'What do you want to do next?',
-        choices: ['Add an Engineer', 'Add an Intern', 'Finish building the team', 'Medium', 'Small', 'Micro'],
-       
-    },
+    
+  ])
+  .then(answers => {
+    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    teamArr.push(manager);
+    renderChoices();
+   
+  });
 
   
+function renderChoices()
+{
+inquirer
+  .prompt([
 
+    {
+      type: 'rawlist',
+      name: 'option',
+      message: 'What do you want to do next?',
+      choices: ['Add an Engineer', 'Add an Intern', 'Finish building the team'],
+ 
+    }
+])
 
+{
+  if (answers.option == 1)
+  {
+      inputEngineer();
+  }
+  else if (answers.option == 2)
+  {
+      inputIntern();
+  }
+  else 
+      buildHTML();
+  }
+}
+
+  function inputEngineer()
+  {
+    inquirer
+  .prompt([
+    {
+      name: 'name',
+      message: 'Please enter Engineer Name',
+
+    },
+    {
+      name: 'id',
+      message: 'Please enter Engineer ID',
+    },
+    {
+      name: 'email',
+      message: 'Please enter email',
+    },
+    {
+      name:'gitHub',
+      message: 'Please enter GitHub username'
+    },
+  ])
+  .then(answers => {
+    const engineer = new Engineer(answers.name, answers.id, answers.email, answers.gitHub)
+    teamArr.push(engineer);
+    renderChoices();
+
+     });
+  }
+
+  function inputIntern()
+  {
+    inquirer
+  .prompt([
+    {
+      name: 'name',
+      message: 'Please enter Intern Name',
+
+    },
+    {
+      name: 'id',
+      message: 'Please enter Intern ID',
+    },
+    {
+      name: 'email',
+      message: 'Please enter email',
+    },
+    {
+      name:'school',
+      message: 'Please enter name of school'
+    },
   ])
   .then(answers => {
 
-    if (answers.option == 1)
-    {
-        generateEngineer();
-    }
-    else if (answers.option == 2)
-    {
-        generateIntern();
-    }
-    else 
-        buildTeam();
-    // const textData = `<!DOCTYPE html>
-    //                   <html>
-    //                   <head>
-    //                   <link rel="stylesheet" href="style.css">
-    //                   </head>
-    //                   <body>
-    //                   <h1> Name:   ${answers.name}  </h1><br>
-    //                   <b>Location:</b>   ${answers.location}  <br>
-    //                   <b>Phone Number:</b>   ${answers.phone}  <br>
-    //                   <b>GitHub:</b>   ${answers.github}  <br><br>
-    //                   <b>Skills:</b>   ${answers.skills}  <br><br>
-    //                   </body>
-    //                   </html>`
+    const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+    teamArr.push(intern);
+    renderChoices();
 
+     });
+    }
 
-    // const fileName = `${answers.name}.html`;
-    // fs.writeFile(fileName, textData, (err) => {
-    //   if (err) {
-    //     console.log(err);
-    //   }
-    //   console.log(`HTML Report ${fileName} has been created!`);
-    // })
-  });
+  function buildHTML()
+  {}
+
+}
